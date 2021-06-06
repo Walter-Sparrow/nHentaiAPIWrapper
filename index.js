83e077c0791d5528d0105b13bf0cfc85a895d58e
Walter-Sparrow <53474572+Walter-Sparrow@users.noreply.github.com>
2021-06-06 22:24:07 +0300

    small refactoring

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
-class Request {
-  _urlProxy = "https://secret-ocean-49799.herokuapp.com/";
-  _baseUrl = "https://nhentai.net";
+const baseUrl = "https://nhentai.net";
+let isProxyOn = false;
+let proxy = "https://secret-ocean-49799.herokuapp.com/";
 
+class Request {
   constructor(props) {
-    this.isProxyOn = props.isProxyOn;
-    this.urlProxy = props.urlProxy ?? this._urlProxy;
+    isProxyOn = props.isProxyOn ?? isProxyOn;
+    proxy = props.urlProxy ?? proxy;
   }
 
   #url() {
-    return (this.isProxyOn ? this.urlProxy : "") + _baseUrl;
+    return (isProxyOn ? proxy : "") + baseUrl;
+  }
+
+  async apiRequest(url, errorMessage) {
+    return fetch(url)
+      .then((response) => {
+        if (response.status == 404 || response.status == 403)
+          return errorMessage;
+        return response.json();
+      })
+      .catch((err) => {
+        throw err;
+      });
   }
 
   async GetDoujin(id) {
-    let galleryUrl = `${this.#url}/api/gallery/${id}`;
-    return fetch(galleryUrl)
-      .then((response) => {
-        if (response.status == 404 || response.status == 403)
-          return "Doujin not found";
-        return response.json();
-      })
-      .catch((err) => {
-        throw err;
-      });
+    let galleryUrl = `${this.#url()}/api/gallery/${id}`;
+    return this.apiRequest(galleryUrl, "Doujin not found");
   }
 
   async GetRelated(id) {
-    let relatedUrl = `${this.#url}/api/gallery/${id}/related`;
-    return fetch(relatedUrl)
-      .then((response) => {
-        if (response.status == 404 || response.status == 403)
-          return "No related found";
-        return response.json();
-      })
-      .catch((err) => {
-        throw err;
-      });
+    let relatedUrl = `${this.#url()}/api/gallery/${id}/related`;
+    return this.apiRequest(relatedUrl, "No related found");
   }
 }
 
