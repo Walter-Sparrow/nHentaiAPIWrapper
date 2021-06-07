fb0cdb302a5ff1679f42c82bed09c035bab17102
Walter-Sparrow <53474572+Walter-Sparrow@users.noreply.github.com>
2021-06-07 23:10:16 +0300

    moved non-related functions from the class

--- index.js
+++ index.js
@@ -2,57 +2,84 @@ const baseUrl = "https://nhentai.net";
 let isProxyOn = false;
 let proxy = "https://secret-ocean-49799.herokuapp.com/";
 
+async function apiRequest(url, errorMessage) {
+  return fetch(url)
+    .then((response) => {
+      if (response.status == 404 || response.status == 403) return errorMessage;
+      return response.json();
+    })
+    .catch((err) => {
+      throw err;
+    });
+}
+
+async function htmlRequest(url, errorMessage) {
+  return fetch(url)
+    .then((response) => {
+      if (response.status == 404 || response.status == 403) return errorMessage;
+      return response.text();
+    })
+    .catch((err) => {
+      throw err;
+    });
+}
+
+function GetDoujinFromHTMLElement(doujin) {
+  const id = doujin
+    .querySelector(".cover")
+    .getAttribute("href")
+    .match(/(?<=\/g\/).+(?=\/)/);
+  const tags = doujin.getAttribute("data-tags");
+  const title = doujin.querySelector(".caption").innerHTML;
+  const thumb = doujin.querySelector(".cover > img");
+  const language = tags.includes("6346")
+    ? "japanese"
+    : tags.includes("12227")
+    ? "english"
+    : tags.includes("29963")
+    ? "chinese"
+    : undefined;
+  return {
+    id: id[0],
+    title,
+    language,
+    thumbnail: {
+      s:
+        thumb.getAttribute("data-src") ||
+        thumb.getAttribute("src").replace(/^\/\//, "https://"),
+      w: thumb.getAttribute("width"),
+      h: thumb.getAttribute("height"),
+    },
+  };
+}
+
+function getUrl() {
+  return (isProxyOn ? proxy : "") + baseUrl;
+}
+
 class Request {
   constructor(props) {
     isProxyOn = props.isProxyOn ?? isProxyOn;
     proxy = props.urlProxy ?? proxy;
   }
 
-  #url() {
-    return (isProxyOn ? proxy : "") + baseUrl;
-  }
-
-  async apiRequest(url, errorMessage) {
-    return fetch(url)
-      .then((response) => {
-        if (response.status == 404 || response.status == 403)
-          return errorMessage;
-        return response.json();
-      })
-      .catch((err) => {
-        throw err;
-      });
-  }
-
-  async htmlRequest(url, errorMessage) {
-    return fetch(url)
-      .then((response) => {
-        if (response.status == 404 || response.status == 403)
-          return errorMessage;
-        return response.text();
-      })
-      .catch((err) => {
-        throw err;
-      });
-  }
-
   async GetDoujin(id) {
-    let galleryUrl = `${this.#url()}/api/gallery/${id}`;
-    return this.apiRequest(galleryUrl, "Doujin not found");
+    let galleryUrl = `${getUrl()}/api/gallery/${id}`;
+    return apiRequest(galleryUrl, "Doujin not found");
   }
 
   async GetRelated(id) {
-    let relatedUrl = `${this.#url()}/api/gallery/${id}/related`;
-    return this.apiRequest(relatedUrl, "No related found");
+    let relatedUrl = `${getUrl()}/api/gallery/${id}/related`;
+    return apiRequest(relatedUrl, "No related found");
   }
 
-  async GetPopular() {
+  async GetPopularNow() {
     const result = [];
-    this.htmlRequest(this.#url(), "test").then((html) => {
+    htmlRequest(getUrl(), "test").then((html) => {
       const page = document.createElement("html");
       page.innerHTML = html;
       page.querySelectorAll(".index-popular .gallery").forEach((doujin) => {
-        result.push(this.GetDoujinFromHTMLElement(doujin));
+        result.push(GetDoujinFromHTMLElement(doujin));
       });
     });
     return result;
@@ -60,46 +87,17 @@ class Request {
 
   async GetNew() {
     const result = [];
-    this.htmlRequest(this.#url(), "test").then((html) => {
+    htmlRequest(getUrl(), "test").then((html) => {
       const page = document.createElement("html");
       page.innerHTML = html;
       page
         .querySelectorAll(".index-container .gallery")
         .forEach((doujin, index) => {
           if (index < 5) return;
-          result.push(this.GetDoujinFromHTMLElement(doujin));
+          result.push(GetDoujinFromHTMLElement(doujin));
         });
     });
     return result;
-  }
-
-  GetDoujinFromHTMLElement(doujin) {
-    const id = doujin
-      .querySelector(".cover")
-      .getAttribute("href")
-      .match(/(?<=\/g\/).+(?=\/)/);
-    const tags = doujin.getAttribute("data-tags");
-    const title = doujin.querySelector(".caption").innerHTML;
-    const thumb = doujin.querySelector(".cover > img");
-    const language = tags.includes("6346")
-      ? "japanese"
-      : tags.includes("12227")
-      ? "english"
-      : tags.includes("29963")
-      ? "chinese"
-      : undefined;
-    return {
-      id: id[0],
-      title,
-      language,
-      thumbnail: {
-        s:
-          thumb.getAttribute("data-src") ||
-          thumb.getAttribute("src").replace(/^\/\//, "https://"),
-        w: thumb.getAttribute("width"),
-        h: thumb.getAttribute("height"),
-      },
-    };
   }
 }
 
