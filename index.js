8167a159eb0604314ea6d5203398375e5ec32a03
Walter-Sparrow <53474572+Walter-Sparrow@users.noreply.github.com>
2021-06-07 14:19:11 +0300

    added GetNew

--- index.js
+++ index.js
@@ -52,35 +52,54 @@ class Request {
       const page = document.createElement("html");
       page.innerHTML = html;
       page.querySelectorAll(".index-popular .gallery").forEach((doujin) => {
-        const id = doujin
-          .querySelector(".cover")
-          .getAttribute("href")
-          .match(/(?<=\/g\/).+(?=\/)/);
-        const tags = doujin.getAttribute("data-tags");
-        const title = doujin.querySelector(".caption").innerHTML;
-        const thumb = doujin.querySelector(".cover > img");
-        const language = tags.includes("6346")
-          ? "japanese"
-          : tags.includes("12227")
-          ? "english"
-          : tags.includes("29963")
-          ? "chinese"
-          : undefined;
-        result.push({
-          id: id[0],
-          title,
-          language,
-          thumbnail: {
-            s:
-              thumb.getAttribute("data-src") ||
-              thumb.getAttribute("src").replace(/^\/\//, "https://"),
-            w: thumb.getAttribute("width"),
-            h: thumb.getAttribute("height"),
-          },
-        });
+        result.push(this.GetDoujinFromHTMLElement(doujin));
       });
     });
     return result;
+  }
+
+  async GetNew() {
+    const result = [];
+    this.htmlRequest(this.#url(), "test").then((html) => {
+      const page = document.createElement("html");
+      page.innerHTML = html;
+      page
+        .querySelectorAll(".index-container .gallery")
+        .forEach((doujin, index) => {
+          if (index < 5) return;
+          result.push(this.GetDoujinFromHTMLElement(doujin));
+        });
+    });
+    return result;
+  }
+
+  GetDoujinFromHTMLElement(doujin) {
+    const id = doujin
+      .querySelector(".cover")
+      .getAttribute("href")
+      .match(/(?<=\/g\/).+(?=\/)/);
+    const tags = doujin.getAttribute("data-tags");
+    const title = doujin.querySelector(".caption").innerHTML;
+    const thumb = doujin.querySelector(".cover > img");
+    const language = tags.includes("6346")
+      ? "japanese"
+      : tags.includes("12227")
+      ? "english"
+      : tags.includes("29963")
+      ? "chinese"
+      : undefined;
+    return {
+      id: id[0],
+      title,
+      language,
+      thumbnail: {
+        s:
+          thumb.getAttribute("data-src") ||
+          thumb.getAttribute("src").replace(/^\/\//, "https://"),
+        w: thumb.getAttribute("width"),
+        h: thumb.getAttribute("height"),
+      },
+    };
   }
 }
 
